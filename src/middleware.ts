04b9881d2e250681b94cecdb7162417e10d3e687
kapil <roberthoodhood2@gmail.com>
2024-11-09 14:24:23 +0530

    public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,18 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher(['/'])
 
+
 export default clerkMiddleware(async (auth, req) => {
+
+  
   if (isProtectedRoute(req)) await auth.protect()
+
 })
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Exclude specific paths like `/api/webhooks/clerk` from Clerk's middleware
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/(api|trpc)(?!/webhooks/clerk)(.*)', // Bypasses Clerk protection for `/api/webhooks/clerk`
   ],
-}
+};
